a08f11a9d42d433c28df191493717590ef39a540
PopoGonry <59679349+PopoGonry@users.noreply.github.com>
2025-05-08 19:23:07 +0900

    2025-05-08
    테스트 전

--- src/main/java/com/popogonry/killEffectPlugin/killEffect/KillEffectCommand.java
+++ src/main/java/com/popogonry/killEffectPlugin/killEffect/KillEffectCommand.java
@@ -10,20 +10,42 @@ public class KillEffectCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
 
+
+        KillEffectService killEffectService = new KillEffectService();
+
         if(strings.length == 1) {
             if(commandSender.isOp()) {
-//                if(strings[0].equalsIgnoreCase("show")) {
-//                    commandSender.sendMessage(ShopRepository.shopDataHashMap.toString());
-//                    commandSender.sendMessage(ShopRepository.shopNameSet.toString());
-//                    return true;
-//                }
                 if(strings[0].equalsIgnoreCase("list")) {
-
+                    killEffectService.printKillEffectList((Player) commandSender);
                 }
             }
         }
+        else if(strings.length == 2) {
+            if(commandSender.isOp()) {
+                if(strings[0].equalsIgnoreCase("add")) {
+                    if(killEffectService.createKillEffect("name", "mysticmobName", "lore", 10.0, KillEffectActiveType.ALL)) {
+                        commandSender.sendMessage("추가되었습니다.");
+                    }
+                    else {
+                        commandSender.sendMessage("추가 실패했습니다.");
+                    }
+                }
+                else if(strings[0].equalsIgnoreCase("remove")) {
+                    if(killEffectService.removeKillEffect("name")) {
+                        commandSender.sendMessage("삭제되었습니다.");
+                    }
+                }
+                else if(strings[0].equalsIgnoreCase("update")) {
+                    if(killEffectService.updateKillEffect("name", new KillEffect("name2", "mysticmobName2", "lore2", 10.0, KillEffectActiveType.ALL))) {
+                        commandSender.sendMessage("수정되었습니다.");
+                    }
+                    else {
+                        commandSender.sendMessage("수정 실패했습니다.");
+                    }
+                }
 
-
+            }
+        }
 
         return false;
     }
